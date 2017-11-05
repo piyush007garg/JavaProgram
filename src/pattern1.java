d2bab4e6b219201c89486bf84ee6bd2957af6986
DELL <DELL@DESKTOP-0F2R76M>
2017-11-05 11:39:39 +0530

    pattern 1

---
+++ src/pattern1.java
@@ -0,0 +1,20 @@
+
+public class pattern1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		// int n=1;
+	        for(int i=1; i<=5; i++)
+	        {
+	        	 int n=1;
+	            for(int j=1; j<=i; j++)
+	            {
+	                System.out.print(n+ " ");
+	                n++;
+	            }
+	           // n++;
+	            System.out.println();
+	        }
+	}
+
+}
